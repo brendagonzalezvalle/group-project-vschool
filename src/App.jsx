d7df8e2394b8b03d90805611c5b0b46dc9e664e5
Mark Faulkner <mark.faulkner976@gmail.com>
2023-02-13 12:43:14 -0600

    initial meme setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css'
 import Header from './Header'
+import Meme from './Meme'
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
       <h1> App is working !!</h1>
     </div>
   )
---
+++ src/Meme.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Meme() {
+    return (
+        <h1>Meme is working</h1>
+    )
+}
