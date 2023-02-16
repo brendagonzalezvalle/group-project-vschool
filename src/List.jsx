effbf33a20f9c316bed7388fe1edceecb75488a1
domniea <domniea@gmail.com>
2023-02-16 15:50:55 -0700

    Edit and Delete Button Functional

--- src/List.jsx
+++ src/List.jsx
@@ -1,21 +1,71 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useState } from "react";
 
 
 export default function List(props){
+    const [isShown, setIsShown] = useState(true)
+    const [showEdit, setShowEdit] = useState(false)
+    const [listItem, setListItem] = useState(
+        {
+            topText: props.topText,
+            bottomText: props.bottomText,
+            randomImage: props.randomImage
+        }
+    )
 
-    function handleClick() {
-        console.log('i work')
+    function handleChange() {
+        const {name, value} = event.target
+        setListItem(prevState => {
+            return (
+                {
+                    ...prevState,
+                    [name]: value
+                }
+            )
+        })
+    }
+
+    function showEditInput() {
+        // console.log('i work')
+        setShowEdit(prevState => !prevState)
+    }
+
+    function deleteMeme() {
+        return setIsShown(prevState => !prevState)
     }
 
     return (
         <div className="list--container">
-            <div className="list-objects">
-                <h2 className="list--topText">{props.topText}</h2>
-                <img className='memeImg'  src={props.randomImage} />
-                <h2 className="list--bottomText" >{props.bottomText}</h2>
-            </div>
-                <button onClick={handleClick}>Edit</button>
-                <button>Delete</button>
+            {isShown &&
+                <Fragment>
+                    <div className="list-objects">
+                        <h2 className="list--topText">{listItem.topText}</h2>
+                        <img className='memeImg'  src={listItem.randomImage} />
+                        <h2 className="list--bottomText" >{listItem.bottomText}</h2>
+                    </div>
+                    {
+                        showEdit &&
+                        <Fragment>
+                            <input
+                                name='topText'
+                                onChange={handleChange}
+                                value={listItem.topText}
+                            >
+                            </input>
+                            <input
+                                name='bottomText'
+                                onChange={handleChange}
+                                value={listItem.bottomText}
+                            >
+                            </input>
+                        </Fragment>
+                    }
+                        <button onClick={showEditInput}>
+                            {showEdit ? 'Save': 'Edit'}
+                        </button>
+                        <button onClick={deleteMeme}>Delete</button>
+                </Fragment>
+            }
         </div>
     )
 }
